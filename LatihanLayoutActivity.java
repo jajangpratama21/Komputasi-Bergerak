d52770ac8e5cd186ab56a91d6868df2e64dc7995
jajangpratama21 <45492984+jajangpratama21@users.noreply.github.com>
2019-03-20 13:19:30 +0700

    Add files via upload

---
+++ LatihanLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.testing.latihan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LatihanLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latihan_layout);
+    }
+}
